038c7d1973f6ac9beca3b5ba2f5606369665dbdf
SergioG93 <60491407+SergioG93@users.noreply.github.com>
2022-02-21 09:25:04 -0800

    Proyecto

---
+++ Colegio CBC/colegio/cbc/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once "controladores/plantilla.controlador.php";
+require_once "controladores/usuarios.controlador.php";
+
+require_once "controladores/aspirantes.controlador.php";
+require_once "controladores/asignacion.controlador.php";
+
+require_once "modelos/usuarios.modelo.php";
+
+require_once "modelos/aspirantes.modelo.php";
+require_once "modelos/asignacion.modelo.php";
+
+require_once "modelos/categorias.modelo.php";
+require_once "controladores/categorias.controlador.php";
+
+
+
+
+
+$plantilla = new ControladorPlantilla();
+$plantilla -> ctrPlantilla();
